dde988a9d22dd9c28ea2a82a18c0ff3139b9c96d
nickzylstra <nick.zylstra@gmail.com>
2019-11-01 14:48:11 -0700

    Adds messageCleaning file

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -1,11 +1,13 @@
 var MessagesView = {
-
   $chats: $('#chats'),
 
-  initialize: function() {
+  initialize: function initialize() {
   },
 
-  render: function() {
-  }
+  render: function render() {
+  },
 
+  renderMessage: function renderMessage(message) {
+    this.$chats.prepend();
+  },
 };
